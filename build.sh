25b5316dc007594205c9c0e30ef2e0c9adf3b44c
Abhijeet Sudhir <asudhir@umich.edu>
2025-03-01 11:05:35 -0500

    changed build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
+
 set -o errexit
 
 pip install -r requirements.txt
 
-# Initialize the database (if needed)
 python -c "from app import app, db; app.app_context().push(); db.create_all()"
